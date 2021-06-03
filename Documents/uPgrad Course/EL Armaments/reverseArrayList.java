3a1a09a3343572de25198e316ad33b5a440eedad
NishadGawde <nishad.gawde21@gmail.com>
2021-06-04 01:13:18 +0530

    First Commit

---
+++ Documents/uPgrad Course/EL Armaments/reverseArrayList.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class reverseArrayList {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++)
+            list.add(s.nextInt());
+        reverseArrayList(list);
+        for (int i = 0; i < list.size(); i++)
+            System.out.print(list.get(i) + " ");
+    }
+
+    // Method to reverse the ArrayList
+    static void reverseArrayList(ArrayList<Integer> list) {
+         Collections.reverse(list);
+        
+
+        // Write your code here
+    }
+}
